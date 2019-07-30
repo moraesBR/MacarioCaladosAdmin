8004c0f57b5ec87c70381260aaca24ef58ebf96b
Albert Richard <albert.richard@gmail.com>
2019-07-30 20:55:22 -0300

    Models: Sapato e afins

---
+++ app/src/main/java/senac/macariocalcadosadmin/models/MD5.java
@@ -0,0 +1,42 @@
+package senac.macariocalcadosadmin.models;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public abstract class MD5 {
+    public static String md5(String input){
+        try {
+
+            // Static getInstance method is called with hashing MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // digest() method is called to calculate message digest
+            //  of an input digest() return array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String md5(Sapato sapato){
+        return md5(sapato.getNome()
+                +sapato.getTipo().toString()
+                +sapato.getGenero().toString()
+                +sapato.getIdade().toString()
+                +sapato.getTamanho().toString());
+    }
+}
