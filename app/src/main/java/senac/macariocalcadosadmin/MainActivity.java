40c960449410eaf7543ae694d847e920038fa2a4
Albert R. Moraes L <albert.richard@gmail.com>
2019-07-30 14:54:03 -0300

    Tela de login sem bind

--- app/src/main/java/senac/macariocalcadosadmin/MainActivity.java
+++ app/src/main/java/senac/macariocalcadosadmin/MainActivity.java
@@ -3,12 +3,28 @@ package senac.macariocalcadosadmin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout rellay1;
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rellay1 = findViewById(R.id.rellay1);
+
+        handler.postDelayed(runnable, 2000);
     }
 }
