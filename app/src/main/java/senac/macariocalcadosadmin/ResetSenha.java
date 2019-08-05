4cc24c45ceddadfa023338da94cf57bd486d9bfb
Albert R. Moraes L <albert.richard@gmail.com>
2019-08-05 12:10:28 -0300

    Código parcialmente comentado

--- app/src/main/java/senac/macariocalcadosadmin/ResetSenha.java
+++ app/src/main/java/senac/macariocalcadosadmin/ResetSenha.java
@@ -23,6 +23,16 @@ public class ResetSenha extends AppCompatActivity {
     private Toolbar toolbar;
     private FirebaseAuth auth;
 
+/* ---------------------------------- Ciclo de Vida do App -------------------------------------- */
+    /*
+     *  Métodos
+     *      onStart(): Restaura a comunicação com o banco de autenticação
+     *      onCreate(): Inicializa o app (data binding e controle)
+     *      onSaveInstanceState():
+     *      onRestoreInstanceState():
+     */
+
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -37,6 +47,14 @@ public class ResetSenha extends AppCompatActivity {
         eventClick();
     }
 
+/* ---------------------------------- Métodos Data Binding -------------------------------------- */
+    /*
+     *   Métodos:
+     *       bindView(): Realiza a ligação dos elementos do layout com variáveis programáveis.
+     *       splashScreen(): Apresenta a tela de apresentação e faz a transição para a tela
+     *           inicial do app.
+     */
+
     private void bindView(){
         etEmail = findViewById(R.id.et_reset_senha);
         btnResetPassword = findViewById(R.id.btn_reset_senha);
@@ -49,6 +67,14 @@ public class ResetSenha extends AppCompatActivity {
         }catch (NullPointerException e){}
     }
 
+/* ---------------------------------- Métodos de Controles -------------------------------------- */
+    /*
+     *  Métodos
+     *      eventClick(): Controla as ações de cliques dos botões e afins
+     *      resetPassword(): Envia um email para o usuário com a solicitação de reconfiguração de
+     *          email.
+     */
+
     private void eventClick() {
         btnResetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +86,7 @@ public class ResetSenha extends AppCompatActivity {
                 else{
                     etEmail.setError(getResources().getString(R.string.email_alerta));
                 }
+                cleanFields();
             }
         });
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -71,14 +98,14 @@ public class ResetSenha extends AppCompatActivity {
     }
 
     /*
-        resetPassword(): tenta-se enviar um email de reconfiguração de senha para o endereço contido
-        no objeto String, passado como parâmetro do método sendPasswordResetEmail(). Aguarda-se a
-        resposta, que será obtida pelo método addOnCompleteListener, sendo que este a obtém via
-        Listener OnCompleteListener e o contexto de pacote (ResetSenha.this). Este Listener contém a
-        resposta esperada no objeto Task<AuthResult>. Neste caso, se obteve-se sucesso no envio do
-        email (task.isSuccessful()), então emite-se um alerta positivo; caso contrário, emite-se um
-        alerta negativo.
-    */
+     *  resetPassword(): tenta-se enviar um email de reconfiguração de senha para o endereço contido
+     *      no objeto String, passado como parâmetro do método sendPasswordResetEmail(). Aguarda-se
+     *      a resposta, que será obtida pelo método addOnCompleteListener, sendo que este a obtém
+     *      via Listener OnCompleteListener e o contexto de pacote (ResetSenha.this). Este Listener
+     *      contém a resposta esperada no objeto Task<AuthResult>. Neste caso, se obteve-se sucesso
+     *      no envio do email (task.isSuccessful()), então emite-se um alerta positivo; caso
+     *      contrário, emite-se um alerta negativo.
+     */
     private void resetPassword(String email) {
         auth.sendPasswordResetEmail(email)
                 .addOnCompleteListener(ResetSenha.this, new OnCompleteListener<Void>() {
@@ -95,8 +122,19 @@ public class ResetSenha extends AppCompatActivity {
                 });
     }
 
+/* ---------------------------------- Métodos auxiliares ------------------------------------- */
+    /*
+     *   Métodos:
+     *       alert(): Apresenta uma String de alerta na tela.
+     *       cleanFields(): limpa os campos editáveis existentes na tela.
+     */
+
     private void alert(int msg){
         Toast.makeText(ResetSenha.this,msg,Toast.LENGTH_SHORT).show();
+    }
+
+    private void cleanFields(){
+        etEmail.setText("");
     }
 
 }
